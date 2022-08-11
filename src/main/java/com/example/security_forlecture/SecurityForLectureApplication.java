60c550d7e0bd8d47ebc148e554b9ac0b0f63b4fe
xanqus <sbk98031816@gmail.com>
2022-08-11 23:11:40 +0900

    1강 환경설정

---
+++ src/main/java/com/example/security_forlecture/SecurityForLectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.security_forlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityForLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityForLectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/security_forlecture/SecurityForLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security_forlecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityForLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
