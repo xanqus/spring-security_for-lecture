643a2fe4215a76d4fefd3dd6c778a6d709eb0758
xanqus <sbk98031816@gmail.com>
2022-08-17 09:51:09 +0900

    6강 시큐리티 권한처리

--- src/main/java/com/example/security_forlecture/config/SecurityConfig.java
+++ src/main/java/com/example/security_forlecture/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.security_forlecture.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity// 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됩니다.
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured annotation 활성화, preAuthorize annotation 활성화
 public class SecurityConfig {
 
     // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
--- src/main/java/com/example/security_forlecture/controller/IndexController.java
+++ src/main/java/com/example/security_forlecture/controller/IndexController.java
@@ -3,11 +3,17 @@ package com.example.security_forlecture.controller;
 import com.example.security_forlecture.dao.UserRepository;
 import com.example.security_forlecture.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class IndexController {
@@ -19,7 +25,10 @@ public class IndexController {
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping({"","/"})
-    public String index() {
+    public String index(@AuthenticationPrincipal UserDetails userDetails) {
+        if(userDetails!=null) {
+            System.out.println(userDetails.getUsername());
+        }
         return "index";
     }
 
@@ -63,5 +72,18 @@ public class IndexController {
         user.setPassword(encPassword);
         userRepository.save(user);
         return "redirect:/loginForm";
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+    @GetMapping("/info")
+    public @ResponseBody String info() {
+        return "개인정보";
+    }
+
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/user_info_data")
+    public @ResponseBody String user_info_data() {
+        return "민감한 정보";
     }
 }
